0175e47c83acb989b169436d4952dc3b1feff5e5
Maxime Mezrahi <maxime.mezrahi@gmail.com>
2014-02-03 11:55:01 +0100

    Algorithm improved

--- src/angular-static-route.js
+++ src/angular-static-route.js
@@ -1,53 +1,72 @@
 (function(angular) {
-    'use strict';
+	'use strict';
 
-    angular.module('angular-static-route', [])
+	var history = [];
+
+	angular.module('angular-static-route', [])
 	.config(function ($routeProvider) {
 
 		var originalWhen = $routeProvider.when;
 		var firstLoad = true;
 		var routesConfig = {};
+		var mainRoute = null;
 
-		var staticRoute = function($q, $route, $rootScope, $window) {
-		    var deferred = $q.defer();
-		    var route = $route.current.$$route;
-		    var sr = (!route.defaultParent || !route.event) ? false : true;
+		var staticRoutePromise = function($q, $route, $rootScope, $window, $location) {
+			var deferred = $q.defer();
+			var route = $route.current.$$route;
+			var sr = (route.event && route.defaultParent) ? true : false;
+			var sor = false;
 
-		    var done = function() {
-			$window.history.back();
-		    };
+			if (history.length >= 2) {
+				var item = routesConfig[history[history.length - 2]];
+				if (item && item.event && item.defaultParent) {
+					//Blocking view
+					sor = true;
+					if (item.onChange) $rootScope.$broadcast(item.onChange);
+				}
+			}
 
-		    // If it is a static route, we call the event
-		    if (sr)
-			$rootScope.$broadcast(route.event, done);
+			// If it is a static route, we call the event
+			if (sr)
+				$rootScope.$broadcast(route.event);
 
-		    // If that's the first load or that's not a static route, we resolve the promise to display the view
-		    if (firstLoad || !sr)
-			deferred.resolve('');
+			// If that's the first load or that's not a static route, we resolve the promise to display the view
+			if (firstLoad || (!sr && !sor)) {
+				deferred.resolve('');
+			}
+			if (firstLoad)
+				firstLoad = false;
 
-		    return deferred.promise;
+			return deferred.promise;
 		};
-
 
 		$routeProvider.when = function(path, config) {
-		    routesConfig[path] = config;
-		    if (!config.resolve) config.resolve = {};
-		    config.resolve.$staticRoute = staticRoute;
-		    if (config.defaultParent && config.event) {
-			var like = routesConfig[config.defaultParent];
-			if (like) {
-			    config.template = like.template;
-			    config.templateUrl = like.templateUrl;
-			    config.controller = like.controller;
-			    for (var name in like.resolve) {
-				if (name != '$staticRoute');
-				config.resolve[name] = like.resolve[name];
-			    }
+			routesConfig[path] = config;
+			if (!config.resolve) config.resolve = {};
+			config.resolve.$staticRoutePromise = staticRoutePromise;
+			if (config.defaultParent && config.event) {
+				var like = routesConfig[config.defaultParent];
+				if (like) {
+					config.template = like.template;
+					config.templateUrl = like.templateUrl;
+					config.controller = like.controller;
+					for (var name in like.resolve) {
+						if (name != '$staticRoute');
+						config.resolve[name] = like.resolve[name];
+					}
+				}
 			}
-		    }
-		    originalWhen(path, config);
-		    return $routeProvider;
+			originalWhen(path, config);
+			return $routeProvider;
 		};
-  
-	    });
+
+	})
+	.run(function ($rootScope, $route, $location) {
+		
+		$rootScope.$on('$locationChangeSuccess', function(event) {
+			history.push($route.current.$$route.originalPath);
+		});
+
+	});
+
 })(angular);
